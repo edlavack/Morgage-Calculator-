6dbd63e50c9ae80e38e636930b4c192fe849c1cc
edlavack <108073221+edlavack@users.noreply.github.com>
2022-09-23 15:12:56 -0400

    link

--- js/site.js
+++ js/site.js
@@ -11,35 +11,20 @@ function generatePayments(loanAmount, term, interestRate) {
   //this is where the math goes
 
   let mRate = interestRate / 1200;
-  let totalMonthlyPayment = (loanAmount * mRate) / (1 - Math.pow(1 + mRate, -term));
-  let prevRemBalance = loanAmount;
+  let totalMonthlyPayment =
+    (loanAmount * mRate) / (1 - Math.pow(1 + mRate, -term));
   let totalInterest = 0;
   let balance = loanAmount;
   let interestPayment = 0;
   let index = 1;
 
-  let payments = [
-    payment = {
-      month: 0,
-      payment: totalMonthlyPayment,
-      principal: totalMonthlyPayment - prevRemBalance * mRate,
-      interest: prevRemBalance * mRate,
-      totalInterest: prevRemBalance * mRate,
-      balance: loanAmount,
-    },
-  ];
+  let payments = [];
 
-  for (let index = 1; index <= term; index++) {
-    totalInterest = totalInterest + interestPayment;
-    interestPayment = prevRemBalance * mRate;
+  for (index = 1; index <= term; index++) {
+    interestPayment = balance * mRate;
+    totalInterest += interestPayment;
     principalPayment = totalMonthlyPayment - interestPayment;
-    balance = prevRemBalance - principalPayment;
-
-    if (index === 1) {
-      prevRemBalance = loanAmount;
-    } else {
-      prevRemBalance = payments[index - 1].balance;
-    }
+    balance -= principalPayment;
 
     payments.push({
       month: index,
@@ -57,23 +42,22 @@ function displayPayments(payments) {
   let paymentTemplate = document.getElementById("statTemplate");
   let paymentBody = document.getElementById("rateBody");
 
-
-  let formatter = new Intl.NumberFormat('en-US',{
-    style: 'currency',
-    currency: 'USD'
-});
+  let formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 
   for (let index = 0; index < payments.length; index++) {
     let paymentNode = document.importNode(paymentTemplate.content, true);
     let paymentItems = Array.from(paymentNode.querySelectorAll("td"));
 
-   
-
     paymentItems[0].textContent = payments[index].month;
     paymentItems[1].textContent = formatter.format(payments[index].payment);
     paymentItems[2].textContent = formatter.format(payments[index].principal);
     paymentItems[3].textContent = formatter.format(payments[index].interest);
-    paymentItems[4].textContent = formatter.format(payments[index].totalInterest);
+    paymentItems[4].textContent = formatter.format(
+      payments[index].totalInterest
+    );
     paymentItems[5].textContent = formatter.format(payments[index].balance);
 
     paymentBody.appendChild(paymentNode);
